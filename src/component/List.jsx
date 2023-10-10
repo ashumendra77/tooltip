2a4e928ab4d051b5d02076c06654d099f443f561
ashumendra77 <ashumendrapratap77@gmail.com>
2023-10-10 22:28:27 +0530

    starting react

---
+++ src/component/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ListItem() {
+    return <div>
+        <ul>
+            <li>HTML AND CSS</li>
+            <li>node js and Mongodb</li>
+            <li>Javascript and api</li>
+            <li>React</li>
+        </ul>
+    </div>
+}
+
+export default ListItem;
