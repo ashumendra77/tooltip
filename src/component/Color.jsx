4fa2072c1d7a5fec995709fb1e86d59dbd68901c
ashumendra77 <ashumendrapratap77@gmail.com>
2023-10-18 23:17:49 +0530

    changes

--- src/component/Color.jsx
+++ src/component/Color.jsx
@@ -1,10 +1,20 @@
+import ReactTooltip from 'react-tooltip';
+
+
 export default function Colordemo({ props }) {
 
     console.log(props);
+
+    const name = 'Ashu';
+    const lname = 'Singh';
     return (
 
         <div>
+            <h1>Hello {name + " " + lname}</h1>
             <h1>Colors Component</h1>
+            {/* <button>Hover on me!</button> */}
+            <div data-tip="msg to show" data-for='toolTip1' data-place='top'>Tooltip</div>
+            <ReactTooltip id="toolTip1" />
             <ul>
                 {/* {props.map((c) =>
                     <li>{c}</li>
