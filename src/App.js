58b619f839ffc44e041838568c78bb97690fdf96
ashumendra77 <ashumendrapratap77@gmail.com>
2024-01-21 15:40:24 +0530

    remove list

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1 style={{color:"Red"}}>Welcom to React</h1>
-      <ListItem heading="Item List" />
+      {/* <ListItem heading="Item List" /> */}
       <Dice/>
       <Colordemo arr={['red','yellow','pink','brown']}/>
     </div>
